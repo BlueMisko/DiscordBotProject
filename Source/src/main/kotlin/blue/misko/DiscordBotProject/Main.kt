524a573b00820ebc7feb9a8ebd3a2487d7066225
Unknown <psneumn@gmail.com>
2019-10-24 22:15:27 +0200

    Helpful features for rpi
    Fixed pom.xml and added force close command to help rpi check for updates

--- Source/src/main/kotlin/blue/misko/DiscordBotProject/Main.kt
+++ Source/src/main/kotlin/blue/misko/DiscordBotProject/Main.kt
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.*
 import net.dv8tion.jda.api.entities.*
 import net.dv8tion.jda.api.events.message.*
 import net.dv8tion.jda.api.hooks.*
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     var bot = JDABuilder(args[0])
@@ -18,6 +19,9 @@ class Bot: ListenerAdapter() {
             return
         }
         val prefix = GetPrefix(event.guild.id)
+        if(event.author.id=="216095103104712706" && event.message.contentRaw=="Admin stop"){
+            exitProcess(0)
+        }
         if(event.message.contentRaw.startsWith(prefix)){
             var command =event.message.contentRaw.removePrefix(prefix)
             executeCommand(event, command.trim())
